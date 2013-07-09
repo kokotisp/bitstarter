1a3650be8410a896bc1053124696df2f5136cfea
sknipas <sknipas@laptop.(none)>
2013-07-10 00:18:20 +0300

    Updated web.js reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+fs.readFile( "index.html", function (err, data) {
+  if (err) cb( err );
+});
+var data = fs.readFileSync( "index.html" );
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8', 0, data));
 });
 
 var port = process.env.PORT || 5000;
