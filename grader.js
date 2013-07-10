39a8eddd5090537c7b01a42a00a58d4fc6b07b78
sknipas <sknipas@laptop.(none)>
2013-07-11 01:21:27 +0300

    latest commit

--- grader.js
+++ grader.js
@@ -1,31 +1,16 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+Quick hacks little time sorry.
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_DEFAULT = "";
+var URL_DEFAULT = "";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util'),
+    rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,7 +21,26 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(Url) {
+var instr;
+rest.get(Url.toString()).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    instr= result;
+	fs.writeFileSync("index2.html", instr, 'utf8');
+	var checkJson = checkHtmlFile("index2.html", program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+   	 console.log(outJson);
+	fs.unlinkSync("index2.html")
+  }
+
+});
+};
+
 var cheerioHtmlFile = function(htmlfile) {
+
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
@@ -55,6 +59,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +70,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <remote>', 'Remote url', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+
+if (program.file!=""){var checkJson = checkHtmlFile(program.file, program.checks);var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);} 
+
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
